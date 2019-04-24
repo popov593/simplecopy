285d4d48828c38ab823f34a37232c1fddd31fd38
Ivan Popov <39959790+popov593@users.noreply.github.com>
2019-04-24 11:42:26 +0300

    Update simplecopy.php

--- simplecopy.php
+++ simplecopy.php
@@ -61,10 +61,10 @@ if( !function_exists("simplecopy_page") ) {
 function simplecopy_page(){
     
 echo "<h1 class='titlesc' style='padding-bottom: 15px;'>Create a backup via Simple Copy</h1>";
-echo "<p><a class='button buttonsc' href='../wp-content/plugins/simplecopy/backupplugins.php'>Backup plugins</a></p>";
-echo "<p><a class='button buttonsc' href='../wp-content/plugins/simplecopy/backupthemes.php'>Backup themes</a></p>";
+echo "<p><a class='button buttonsc' href='admin.php?page=simplecopy_backupplugin'>Backup plugins</a></p>";
+echo "<p><a class='button buttonsc' href='admin.php?page=simplecopy_backupthemes'>Backup themes</a></p>";
 //echo "<p><a class='button buttonsc'>Backup WP cores</a></p>";
-echo "<p><a class='button buttonsc'  href='../wp-content/plugins/simplecopy/backupdb.php'>Backup database</a></p>";
+echo "<p><a class='button buttonsc'  href='admin.php?page=simplecopy_backupdb'>Backup database</a></p>";
 echo "<p style='padding-bottom: 30px;'></p>";
     
 echo '<h2>Existing Plugins Backups:</h2>';
@@ -85,5 +85,98 @@ echo str_replace("","","<p><a href='$filename'>$filename</a></p>\n");
 }
 echo "<p style='padding-bottom: 30px;'></p>";
 
+}
+}
+
+//Backup plugins page
+//add menu element
+add_action( 'admin_menu', 'simplecopy_backup_menu' );
+
+function simplecopy_backup_menu(){
+  $parent_slug = 'simplecopy';
+  $page_title = 'Plugin backup is being taken';
+  $menu_title = 'Backup Plugins';
+  $capability = 'manage_options';
+  $menu_slug  = 'simplecopy_backupplugin';
+  $function   = 'simplecopy_backup';
+  $icon_url   = 'dashicons-plus-alt';
+  $position   = 2;
+
+  add_submenu_page($parent_slug,
+                 $page_title,
+                 $menu_title,
+                 $capability,
+                 $menu_slug,
+                 $function,
+                 $icon_url,
+                 $position );
+}
+
+if( !function_exists("simplecopy_backup") ) {
+function simplecopy_backup(){
+    
+include('backupplugins.php');
+}
+}
+
+//Backup themes page
+//add menu element
+add_action( 'admin_menu', 'simplecopy_backupthemes_menu' );
+
+function simplecopy_backupthemes_menu(){
+  $parent_slug = 'simplecopy';
+  $page_title = 'Themes backup is being taken';
+  $menu_title = 'Backup Themes';
+  $capability = 'manage_options';
+  $menu_slug  = 'simplecopy_backupthemes';
+  $function   = 'simplecopy_backuptheme';
+  $icon_url   = 'dashicons-plus-alt';
+  $position   = 3;
+
+  add_submenu_page($parent_slug,
+                 $page_title,
+                 $menu_title,
+                 $capability,
+                 $menu_slug,
+                 $function,
+                 $icon_url,
+                 $position );
+}
+
+if( !function_exists("simplecopy_backuptheme") ) {
+function simplecopy_backuptheme(){
+    
+include('backupthemes.php');
+}
+}
+
+//Backup themes page
+//add menu element
+add_action( 'admin_menu', 'simplecopy_backupdb_menu' );
+
+function simplecopy_backupdb_menu(){
+  $parent_slug = 'simplecopy';
+  $page_title = 'DB backup is being taken';
+  $menu_title = 'Backup Database';
+  $capability = 'manage_options';
+  $menu_slug  = 'simplecopy_backupdb';
+  $function   = 'simplecopy_backupdbpage';
+  $icon_url   = 'dashicons-plus-alt';
+  $position   = 3;
+
+  add_submenu_page($parent_slug,
+                 $page_title,
+                 $menu_title,
+                 $capability,
+                 $menu_slug,
+                 $function,
+                 $icon_url,
+                 $position );
+}
+
+if( !function_exists("simplecopy_backupdbpage") ) {
+function simplecopy_backupdbpage(){
+    
+include('backupdb.php');
 }
 }
